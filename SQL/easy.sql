0ea30664dc7458b1f1e9525168957a61c6981a9e
AndreaPiancone97 <111129502+AndreaPiancone97@users.noreply.github.com>
2022-08-14 15:42:31 +0200

    Update easy.sql

--- SQL/easy.sql
+++ SQL/easy.sql
@@ -1,6 +1,13 @@
 --- draw the triangle -1 ------
 
+declare @number as int;
+set @number = 20
 
+while @number != 0
+    begin
+        print(replicate('* ', @number))
+        set @number = @number - 1
+    end
 
 
 ----- draw-the-triangle-2 ----- 
