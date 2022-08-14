f653977d64f8d0b080b37929b07b80a59c835ed3
AndreaPiancone97 <111129502+AndreaPiancone97@users.noreply.github.com>
2022-08-14 18:11:21 +0200

    Update medium.sql

--- SQL/medium.sql
+++ SQL/medium.sql
@@ -12,6 +12,36 @@ GROUP BY LOWER(OCCUPATION)
 ) A 
 ORDER BY CNT, L_OCCUPATION;
 
---- 2) OCCUPATIONS
+--- 2) PRIME NUMBER 
+
+declare @n int
+declare @div int
+declare @cnt int
+declare @res as varchar(1000)
+declare @len_res as int
+
+set @n = 2
+set @res = ''  
+
+while @n <= 1000
+    begin
+    
+        set @cnt = 0
+        set @div = 1
+        
+        while @div <= @n
+            begin
+                if @n % @div = 0
+                    set @cnt = @cnt + 1
+                set @div = @div + 1
+            end
+        
+        if @cnt = 2
+            set @res = concat(@res, @n, '&')
+        set @n = @n + 1
+    end
+
+set @len_res = len(@res) - 1
+print(substring(@res, 1, @len_res))
 
 
