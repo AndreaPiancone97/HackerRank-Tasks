83894f3e6cd67e8ff2200b556bc87b4f9180598d
AndreaPiancone97 <111129502+AndreaPiancone97@users.noreply.github.com>
2022-08-14 20:36:13 +0200

    Update mock_monthly.py

--- python/mock_monthly.py
+++ python/mock_monthly.py
@@ -34,3 +34,37 @@ if __name__ == '__main__':
     arr = list(map(int, input().rstrip().split()))
 
     plusMinus(arr)
+    
+    
+    
+##### 2 Given five positive integers, find the minimum and maximum values that can be calculated by summing exactly four of the five integers. 
+##      Then print the respective minimum and maximum values as a single line of two space-separated long integers.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    sum_values = []
+    for i in range(0, len(arr)):
+        tmp = arr.copy() #with copy method we clone the value of the list without link the junk list with the original one.
+        tmp.pop(i)
+        sum_values.append(sum(tmp))
+    print(min(sum_values),max(sum_values), sep = ' ')
+        
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
