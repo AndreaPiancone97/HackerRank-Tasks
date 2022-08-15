561d0c6f6d54dab3c198bd99074a666214adb4a9
AndreaPiancone97 <111129502+AndreaPiancone97@users.noreply.github.com>
2022-08-15 11:23:03 +0200

    Update medium.sql

--- SQL/medium.sql
+++ SQL/medium.sql
@@ -62,3 +62,37 @@ else
     from (select lat_n, rank() over(order by lat_n asc) as rank from station) as a
     where rank = (@n/2) or rank = (@n/2) + 1
 
+--- the-company
+
+select company.company_code, 
+company.founder, 
+cnt_lead_manager,
+cnt_srn_manager,
+cnt_manager,
+cnt_employee
+
+from Company
+join (select  company_code, count(distinct lead_manager_code) as cnt_lead_manager
+     from Lead_Manager
+      group by company_code
+     ) AS A
+ON A.company_code = company.company_code
+join (
+    select company_code, count(distinct senior_manager_code) as cnt_srn_manager
+    from Senior_Manager
+    group by company_code
+) as B
+on B.company_code = company.company_code
+join (
+    select company_code, count(distinct manager_code) as cnt_manager
+    from Manager
+    group by company_code
+) C
+on C.company_code = company.company_code
+join (
+    select company_code, count(distinct employee_code) as cnt_employee
+    from employee
+    group by company_code  
+) D
+on D.company_code = company.company_code
+ORDER BY company.company_code ASC
